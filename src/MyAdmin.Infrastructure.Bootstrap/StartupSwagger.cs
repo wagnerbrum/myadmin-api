72cbe2ee7ecb64505dc62fea118237c963e1bbc8
Wagner Brum da Silva <wagner.brum@cwi.com.br>
2021-12-20 02:18:48 -0300

    feat(bootstrap): configura swagger na inicialização do projeto em um projeto a parte

---
+++ src/MyAdmin.Infrastructure.Bootstrap/StartupSwagger.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace MyAdmin.Infrastructure.Bootstrap
+{
+    public static class StartupSwagger
+    {
+        private const string UrlBaseSwagger = "/swagger/v1/swagger.json";
+        private const string VersaoSwagger = "v1";
+
+        public static void ConfigureServices(IServiceCollection services, string title, string version, string description)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(VersaoSwagger, new OpenApiInfo { Title = title, Version = version, Description = description });
+            });
+        }
+
+        public static void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint(UrlBaseSwagger, VersaoSwagger));
+        }
+    }
+}
--- src/MyAdmin.WebApi/Startup.cs
+++ src/MyAdmin.WebApi/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Bootstrap = MyAdmin.Infrastructure.Bootstrap;
 
 namespace MyAdmin.WebApi
 {
@@ -23,37 +15,19 @@ namespace MyAdmin.WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAdmin.WebApi", Version = "v1" });
-            });
+            Bootstrap.Startup.ConfigureServices(
+                services,
+                "MyAdmin",
+                "1.0.0", //pegar versão git
+                "Descrição do projeto"
+            );
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAdmin.WebApi v1"));
-            }
-
-            // app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            Bootstrap.Startup.Configure(app, env);
         }
     }
 }
